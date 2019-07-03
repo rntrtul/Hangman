31f333d1dfc64e3d49766aa2b4d92c68b8de8649
Rntrtul <Sistthelord@gmail.com>
2019-07-03 14:48:44 -0400

    added loading and saving games from text file

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,18 @@
 require "rainbow"
+require "oj"
 
 class Game 
-  attr_reader :word_got
+  attr_reader :shown
   attr_reader :secret_word
+  attr_reader :guesses_left
+  attr_reader :letters_guessed
 
   def initialize
     @secret_word = pick_word.downcase.split("")
     @guesses_left = 9
     @shown = "_" * @secret_word.length
     @letters_guessed = []
+    @saving = false
   end
 
   def pick_word
@@ -40,15 +44,19 @@ class Game
   end
 
   def guessing_loop
-    while @guesses_left > 0 && !win_check? do
+    while @guesses_left > 0 && !win_check? && !@saving do
       puts "You have " + Rainbow("#{@guesses_left}").orange + " turns left"
       puts "letters guessed: #{@letters_guessed}"
       puts "you have: #{@shown}"
-      puts "Your guess: "
-      input = gets..downcase.chomp
+      print "Your guess: "
+      input = gets.downcase.chomp
+      puts ""
 
-      if (@letters_guessed.include?(input))
-        puts Rainbow("you already guess it").red
+      if (input == "save")
+        @saving = true
+        save
+      elsif (@letters_guessed.include?(input))
+        puts Rainbow("you already guess it").yellow
         next
       else
         if(guess?(input))
@@ -61,22 +69,67 @@ class Game
     end
   end
 
+  def save 
+    puts "Enter your save name: "
+    file_name = gets.chomp
+    file_path = "saved_games/#{file_name}"
+    Dir.mkdir "saved_games" unless Dir.exists? "saved_games"
+
+    File.open(file_path, 'w') do |file|
+      file.puts Oj.dump self   
+    end
+
+    puts "Game saved"
+  end
+
+  def load 
+    puts "Game saves:"
+    saved_games = Dir.entries("saved_games").reject{|entry| entry == "." || entry == ".."}
+    puts saved_games
+
+    puts "Enter the save you wish to load"
+    file_path = "saved_games/#{gets.chomp}"
+    file = File.open(file_path, 'r')
+
+    load = Oj.load(file)
+    puts "Game loaded"
+
+    @secret_word = load.secret_word
+    @guesses_left = load.guesses_left
+    @shown = load.shown
+    @letters_guessed = load.letters_guessed
+    @saving = false
+  end
+
+
   def win_check? 
      @shown == @secret_word.join("")? true : false
   end
 
+  def final_message
+    if (@saving)
+      puts Rainbow("Game saved succesfully").aqua
+    elsif @guesses_left > 0 
+      puts Rainbow("Congrats you won").green
+      puts "You guessed " + Rainbow(@secret_word.join("")).cyan + " right"
+    else
+      puts "Aw better luck next time"
+      puts "The correct word was " + Rainbow(@secret_word.join("")).cyan
+    end
+  end
+
+
   def game_loop 
     puts "Do you want a new game(1) or load game(2)"
-    if gets.chomp == "1"
+    mode = gets.chomp
+    if mode == "1"
       puts "enter " + Rainbow("save").orange + " anytime to save"
       guessing_loop
-      if @guesses_left > 0 
-        puts Rainbow("Congrats you won").green
-        puts "You guessed " + Rainbow(@secret_word.join("")).cyan + " right"
-      else
-        puts "Aw better luck next time"
-        puts "The correct word was " + Rainbow(@secret_word.join("")).cyan
-      end
+      final_message
+    elsif mode == "2"
+      load
+      guessing_loop
+      final_message
     end
   end
 end
